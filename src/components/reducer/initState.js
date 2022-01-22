4c2985c0f03623a3ee2bfb719d4c7e7746d6d0a4
Dawit Abay <dawitabay@Dawits-MacBook-Pro-2.local>
2022-01-22 15:06:37 -0500

    first commit

---
+++ src/components/reducer/initState.js
@@ -0,0 +1,15 @@
+const initState = {
+    isError: true,
+    postData: {},
+    apiMethod: "GET",
+    baseURLtoDB: "http://localhost:3000/data",
+    baseURLtoDB2: "http://localhost:3000/data2",
+    baseURLtoDB3: "http://localhost:3000/data3",
+    baseURLtoDB4: "http://localhost:3000/data4",
+
+    currentURLtoDB: "http://localhost:3000/data",
+    
+    finalTextResponse: "Hmm..."
+};
+
+export default initState
